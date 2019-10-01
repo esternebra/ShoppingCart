6c447f8b83afd12fc9676df0ddc6d826b5dab0e3
Estela <estelalopezmartin@gmail.com>
2019-10-01 14:06:24 +0200

    Shop and Checkout Components

--- WebShop/src/app/app.module.ts
+++ WebShop/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
--- WebShop/src/app/products.ts
+++ WebShop/src/app/products.ts
@@ -2,7 +2,19 @@ import { Products } from './api';
 
 export const api: Products[] = [
     {
-        
+    id: 'GR1',
+    name: 'Green tea',
+    price: 3.11
+    },
+    {
+    id: 'SR1',
+    name: 'Strawberries',
+    price: 5.00
+    },
+    {
+    id: 'CR1',
+    name: 'Coffee',
+    price: 11.23
     }
 ];
 
